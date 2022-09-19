f17d495c0917dcdb61010ab60ae8213e98f0356e
yoonsi <yoonsi.studio@gmail.com>
2022-09-19 22:23:36 +0900

    post-card의 description, categories 제거

--- src/components/post-card/index.js
+++ src/components/post-card/index.js
@@ -8,16 +8,8 @@ function PostCard({ post }) {
     <div className="post-card-wrapper">
       <Link className="post-card" key={id} to={slug}>
         <div className="title">{title}</div>
-        <p className="description" dangerouslySetInnerHTML={{ __html: excerpt }} />
         <div className="info">
           <div className="date">{date}</div>
-          <div className="categories">
-            {categories.map((category) => (
-              <div className="category" key={category}>
-                {category}
-              </div>
-            ))}
-          </div>
         </div>
       </Link>
     </div>
